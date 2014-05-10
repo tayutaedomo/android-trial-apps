5601cf251a5729aa40099f6119406b9a9d48282e
Noboru Koike <tayutaedomo@gmail.com>
2014-05-10 15:37:56 +0900

    Added a display of touching time.

--- apptouch/src/net/tayutaedomo/apptouch/MyActivity.java
+++ apptouch/src/net/tayutaedomo/apptouch/MyActivity.java
@@ -2,6 +2,7 @@ package net.tayutaedomo.apptouch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -11,6 +12,10 @@ import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+
+import java.math.BigDecimal;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MyActivity extends Activity {
 
@@ -28,6 +33,15 @@ public class MyActivity extends Activity {
     Button touchedButton;
     long then;
 
+    @InjectView(R.id.timerView)
+    TextView timerView;
+
+    final long TOUCH_INTERVAL = 1000;
+
+    float laptime = 0.0f;
+    Timer timer;
+    Handler handler = new Handler();
+
     /**
      * Called when the activity is first created.
      */
@@ -41,19 +55,24 @@ public class MyActivity extends Activity {
         button1.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                Log.d(LOG_TAG, "BUTTON 1 onTouch");
+                Log.d(LOG_TAG, "BUTTON 1 onTouch, action:" + event.getAction());
 
                 if (event.getAction() == MotionEvent.ACTION_UP) {
                     touchedButton = null;
                     then = 0;
                     textView.setText("");
-                } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                    endTimer();
+                }
+                else if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     touchedButton = button1;
                     then = System.currentTimeMillis();
                     textView.setText("BUTTON 1 onTouch");
+
+                    startTimer();
                 }
 
-                if (then != 0 && System.currentTimeMillis() - then > 2000) {
+                if (then != 0 && System.currentTimeMillis() - then > TOUCH_INTERVAL) {
                     showMessage("BUTTON 1");
                 }
 
@@ -64,19 +83,24 @@ public class MyActivity extends Activity {
         button2.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                Log.d(LOG_TAG, "BUTTON 2 onTouch");
+                Log.d(LOG_TAG, "BUTTON 2 onTouch, action:" + event.getAction());
 
                 if (event.getAction() == MotionEvent.ACTION_UP) {
                     touchedButton = null;
                     then = 0;
                     textView.setText("");
-                } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                    endTimer();
+                }
+                else if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     touchedButton = button2;
                     then = System.currentTimeMillis();
                     textView.setText("BUTTON 2 onTouch");
+
+                    startTimer();
                 }
 
-                if (then != 0 && System.currentTimeMillis() - then > 2000) {
+                if (then != 0 && System.currentTimeMillis() - then > TOUCH_INTERVAL) {
                     showMessage("BUTTON 2");
                 }
 
@@ -93,4 +117,38 @@ public class MyActivity extends Activity {
 //    protected void onClickButton1(Button button) {
 //        showMessage("Test Button1");
 //    }
+
+    protected void startTimer() {
+        if(timer == null){
+            //タイマーの初期化処理
+            laptime = 0.0f;
+            timer = new Timer(true);
+
+            timer.schedule( new TimerTask(){
+                @Override
+                public void run() {
+                    handler.post( new Runnable() {
+                        public void run() {
+
+                            laptime +=  0.1d;
+
+                            BigDecimal bi = new BigDecimal(laptime);
+                            float outputValue = bi.setScale(1, BigDecimal.ROUND_HALF_UP).floatValue();
+
+                            timerView.setText(Float.toString(outputValue));
+                        }
+                    });
+                }
+            }, 100, 100);
+        }
+    }
+
+    protected void endTimer() {
+        if(timer != null){
+            timer.cancel();
+            timer = null;
+        }
+
+        timerView.setText("0.0");
+    }
 }
